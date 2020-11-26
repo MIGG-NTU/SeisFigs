949f7d06f050846ed9bf2b991e1f31690df7c79a
core-man <coreman.seism@gmail.com>
2020-11-26 20:03:32 +0800

    test make

--- source/scripts/cpt_seis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gmt begin gmt_seis png,pdf
-gmt makecpt -Cseis -T-6/6/0.5 -Z -D -H > gmt_seis.cpt
-gmt colorbar -Cgmt_seis.cpt -Dx0c/0c+w10c/0.2c+h+e+n -Bxa1
-gmt end
-rm gmt_seis.cpt
